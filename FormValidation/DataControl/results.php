0c919deba918fa8345ce7272797e2a4fef4af452
Md Shahnewaj Sheikh <shahanewajshaikh786@gmail.com>
2022-02-10 00:19:37 +0600

    .

---
+++ FormValidation/DataControl/results.php
@@ -0,0 +1,9 @@
+<?php
+
+$fname= $_POST["fname"];
+echo "Your First name is: ".$fname;
+echo "<br>";
+$lname= $_POST["lname"];
+echo "Your last name is: ".$lname;
+
+?>
---
+++ FormValidation/form.php
@@ -0,0 +1,20 @@
+<head>
+
+<body>
+<h1>Registration Form</h1> <hr>
+<form action="DataControl/results.php" method="post">
+
+Name: 
+<input type="text" name="fname"> 
+Last Name:
+<input type="text" name="lname"> 
+<input type="submit">
+
+ 
+
+
+</form>
+
+</body>
+
+</head>
